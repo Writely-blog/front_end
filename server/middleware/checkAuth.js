f59691a48c88976c64820e2bfe1aaba92668e51e
uch2ha <sinyavskiy.d@gmail.com>
2023-01-19 20:14:31 +0200

    check auth middleware

---
+++ server/middleware/checkAuth.js
@@ -0,0 +1,24 @@
+import * as dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import { StatusCodes } from 'http-status-codes';
+import CustomAPIError from '../errors/custom-error.js';
+
+dotenv.config();
+
+export default (req, res, next) => {
+  const token = (req.headers.authorization || '').replace(/Bearer\s?/, '');
+
+  if (token) {
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      req.userId = decoded.id;
+      next();
+    } catch (err) {
+      console.log(err);
+      throw new CustomAPIError('Not access', StatusCodes.FORBIDDEN);
+    }
+  } else {
+    throw new CustomAPIError('Not access', StatusCodes.FORBIDDEN);
+  }
+};
