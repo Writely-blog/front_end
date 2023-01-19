733aae9fb5ef80fc4750ab629400b5b9ab0c9dc9
uch2ha <sinyavskiy.d@gmail.com>
2023-01-19 19:49:37 +0200

    new db base table command

--- server/db/db-base-table.sql
+++ server/db/db-base-table.sql
@@ -1,3 +1,4 @@
+-- TABLES --
 CREATE TABLE users
 (id BIGSERIAL PRIMARY KEY NOT NULL,
 name VARCHAR(200) NOT NULL,
@@ -13,3 +14,39 @@ likes_count NUMERIC(10) DEFAULT 0,
 user_id INTEGER NOT NULL,
 FOREIGN KEY (user_id) REFERENCES users (id),
 UNIQUE (title));
+
+-- USERS --
+
+INSERT INTO users (name, email, password) VALUES ('Dima', 'dima@dima.com', 'password'); 
+
+-- POSTS --
+
+INSERT INTO posts (title, context, user_id) VALUES ('First Step', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
+similique voluptas rerum ullam, aperiam obcaecati dicta. Nobis inventore porro expedita assumenda voluptates dicta! 
+Quo officia blanditiis quos natus soluta ea amet, laboriosam dolor reprehenderit unde, 
+doloremque tempore eligendi voluptatem dicta non ullam esse quis facilis distinctio officiis ut autem optio debitis! 
+Nulla ratione exercitationem saepe ullam vel nostrum error vitae ea recusandae, nisi ipsum placeat, 
+laudantium facilis similique aperiam dignissimos sequi quaerat assumenda nihil, consequuntur esse quis. Minus modi culpa eos omnis?
+', 1); 
+
+INSERT INTO posts (title, context, user_id) VALUES ('Second Step', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
+similique voluptas rerum ullam, aperiam obcaecati dicta. Nobis inventore porro expedita assumenda voluptates dicta! 
+Quo officia blanditiis quos natus soluta ea amet, laboriosam dolor reprehenderit unde, 
+doloremque tempore eligendi voluptatem dicta non ullam esse quis facilis distinctio officiis ut autem optio debitis! 
+Nulla ratione exercitationem saepe ullam vel nostrum error vitae ea recusandae, nisi ipsum placeat, 
+laudantium facilis similique aperiam dignissimos sequi quaerat assumenda nihil, consequuntur esse quis. Minus modi culpa eos omnis?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
+similique voluptas rerum ullam, aperiam obcaecati dicta. Nobis inventore porro expedita assumenda voluptates dicta! 
+Quo officia blanditiis quos natus soluta ea amet, laboriosam dolor reprehenderit unde, 
+doloremque tempore eligendi voluptatem dicta non ullam esse quis facilis distinctio officiis ut autem optio debitis! 
+Nulla ratione exercitationem saepe ullam vel nostrum error vitae ea recusandae, nisi ipsum placeat, 
+laudantium facilis similique aperiam dignissimos sequi quaerat assumenda nihil, consequuntur esse quis. Minus modi culpa eos omnis?
+Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+Possimus eveniet sed officia soluta hic, error vitae, laborum debitis velit totam temporibus! Earum aliquam nostrum eos hic rem, 
+similique voluptas rerum ullam, aperiam obcaecati dicta. Nobis inventore porro expedita assumenda voluptates dicta! 
+', 1); 
+
+
