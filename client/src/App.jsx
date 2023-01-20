00974adff19d33e6a6a3ca8b121d486d6a8d1706
uch2ha <sinyavskiy.d@gmail.com>
2023-01-20 12:36:33 +0200

    routing is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,34 @@
 import { useState } from 'react';
 import './App.css';
 import TheContextProvider from './TheContext';
+import { Routes, Route, BrowserRouter, Navigate } from 'react-router-dom';
+import Register from './components/register/Register';
+import Login from './components/login/Login';
+import AllPosts from './components/allPosts/AllPosts';
+import MyPosts from './components/myPosts/MyPosts';
 
 function App() {
+  const ProtectedRoute = (props) => {
+    return false ? props.children : <Navigate to='/login' />;
+  };
+
   return (
     <TheContextProvider>
-      <div className='App'></div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<AllPosts />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+          <Route
+            path='/myPosts'
+            element={
+              <ProtectedRoute>
+                <MyPosts />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </TheContextProvider>
   );
 }
