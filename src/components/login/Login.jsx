2372c69ac47dd8ff9024dfe934d83ace8118820c
uch2ha <sinyavskiy.d@gmail.com>
2023-01-20 17:56:29 +0200

    login page is ready

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Login.css';
 import { fetchLogin } from '../../fetchFunctions';
 
 const Login = () => {
@@ -8,10 +9,45 @@ const Login = () => {
   };
 
   return (
-    <div>
-      <button onClick={() => console.log(fetchLogin({ ...data }))}>
-        PRESS
-      </button>
+    <div className='container'>
+      <div className='title'>
+        <p>WRITELY</p>
+      </div>
+      <div className='form-conteiner'>
+        <div className='form-title'>
+          <p>LOGIN</p>
+        </div>
+        <div className='form-body'>
+          <form className='form-container'>
+            <div className='form-inputs'>
+              <div>
+                <input
+                  type='email'
+                  id='email'
+                  name='email'
+                  placeholder='Email'
+                  required
+                />
+              </div>
+              <div>
+                <input
+                  type='password'
+                  id='password'
+                  name='password'
+                  placeholder='Password'
+                  required
+                />
+              </div>
+            </div>
+            <div className='form-btn'>
+              <input type='submit' value='Login' />
+            </div>
+          </form>
+        </div>
+        <div className='form-footer'>
+          <p>Not a member? ...</p>
+        </div>
+      </div>
     </div>
   );
 };
