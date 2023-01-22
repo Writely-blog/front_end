588671da84a1ebd5e05e4966e3b2246b5037d2b9
uch2ha <sinyavskiy.d@gmail.com>
2023-01-22 21:58:14 +0200

    font added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.css';
-import './fonts/Candu/Candu Regular.ttf';
+import './fonts/Candu/CanduRegular.ttf';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
