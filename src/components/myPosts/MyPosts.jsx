16da8c35a56cebd42ffd72674fc4595120118536
uch2ha <sinyavskiy.d@gmail.com>
2023-01-22 21:37:11 +0200

    my post component

--- src/components/myPosts/MyPosts.jsx
+++ src/components/myPosts/MyPosts.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import NavBar from '../navBar/NavBar';
 import OnePost from '../onePost/OnePost';
+import './MyPosts.css';
+import { GrAdd } from 'react-icons/gr';
 
 const MyPosts = () => {
   return (
     <div className='main-container'>
       <NavBar />
+      <div className='my-posts-add-btn'>
+        <div className='my-posts-name'>
+          <p>MY POSTS</p>
+        </div>
+        <div className='my-posts-btn'>
+          <GrAdd size={40} />
+        </div>
+      </div>
       <OnePost />
     </div>
   );
