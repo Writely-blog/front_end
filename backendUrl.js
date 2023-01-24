6e7af5f51a46df44fa0f5a5182eab40d57772451
uch2ha <sinyavskiy.d@gmail.com>
2023-01-24 15:17:58 +0200

    new backend url

--- backendUrl.js
+++ backendUrl.js
@@ -1,3 +1,4 @@
-const BACKEND_URL = 'http://localhost:3030';
+// const BACKEND_URL = 'http://localhost:3030';
+const BACKEND_URL = 'https://backend-production-70b0.up.railway.app/';
 
 export default BACKEND_URL;
