f8af5968b1d31bbc18a379e04a55c7b6825333de
Dmitry Sinyavskiy <89381914+uch2ha@users.noreply.github.com>
2023-05-31 12:12:09 +0300

    Update backendUrl.js

--- backendUrl.js
+++ backendUrl.js
@@ -1,4 +1,4 @@
 // const BACKEND_URL = 'http://localhost:3030';
-const BACKEND_URL = 'https://backend-production-70b0.up.railway.app';
+const BACKEND_URL = import.meta.env.BACKEND_URL;
 
 export default BACKEND_URL;
